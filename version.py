36dd5277da73e73d33c76523135c2202edd91b29
tomsep <52826851+tomsep@users.noreply.github.com>
2025-01-16 21:23:00 +0200

    Customized version.py

--- version.py
+++ version.py
@@ -1,9 +1,12 @@
+# Major.Minor.Patch follows Godot versioning.
+# Custom build is marked using status "physx".
+
 short_name = "godot"
-name = "Godot Engine"
+name = "Godot2DPhysicsExt"
 major = 4
 minor = 5
 patch = 1
-status = "stable"
+status = "physx"
 module_config = ""
 website = "https://godotengine.org"
 docs = "4.5"

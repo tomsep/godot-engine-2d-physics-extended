9a1e07e71a992fc054a5a18a76b19a02dba4baa7
tomsep <52826851+tomsep@users.noreply.github.com>
2025-01-16 22:44:11 +0200

    Changed version.py
    Previous custom version labeling
    was not compatible with NuGet conventions.

--- version.py
+++ version.py
@@ -1,12 +1,12 @@
 # Major.Minor.Patch follows Godot versioning.
-# Custom build is versioned using status "physx-X.Y".
+# Custom build is marked using status "physx".
 
 short_name = "godot"
 name = "Godot2DPhysicsExt"
 major = 4
 minor = 3
 patch = 0
-status = "physx-1.0"
+status = "physx"
 module_config = ""
 website = "https://godotengine.org"
 docs = "4.3"
